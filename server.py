73be9071977407d798d2102361b5e6a184abfe2c
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-01 15:19:55 +0530

    Add Swagger to your POC

--- server.py
+++ server.py
@@ -1,20 +1,19 @@
-from flask import (
-    Flask,
-    render_template
-)
+from flask import render_template
+import connexion
 
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+
 
 # Create a URL route in our application for "/"
-
-
 @app.route('/')
 def home():
     """
     This function just responds to the browser ULR
     localhost:5000/
-
     :return:        the rendered template 'home.html'
     """
     return render_template('home.html')
@@ -22,4 +21,4 @@ def home():
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
