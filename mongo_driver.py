f6e89a1421900094377ac43d1f93c2b3019063ec
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-11-05 20:33:38 +0530

    Remove url and add in another file

--- mongo_driver.py
+++ mongo_driver.py
@@ -8,13 +8,14 @@ A client class which uses pymongo driver to connect to Mongo Atlas Free Tier Acc
 
 class MongoClientConnection(object):
 
-	def __init__(self, db=None, collection=None, username=None, pwd=None):
-		self.database = db
+	def __init__(self, database_name=None, collection=None, username=None, pwd=None, url=None):
+		self.database = database_name
 		self.collection = collection
 		self.client = None
 		self.username = username
 		self.pwd = pwd
-		self.mongo_url = "mongodb://"+self.username+":" + self.pwd + "@cluster0-shard-00-00-kvfhu.mongodb.net:27017,cluster0-shard-00-01-kvfhu.mongodb.net:27017,cluster0-shard-00-02-kvfhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+		self.url = url
+		self.mongo_url = "mongodb://"+self.username+":" + self.pwd + url
 
 	def create_connection(self, database, collection):
 		self.client  = pymongo.MongoClient(self.mongo_url)
