1b6506373141e83744f2380ec7b1175119086076
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-11-05 20:31:49 +0530

    Remove credentials since they are confidential and add methods

--- mongo_driver.py
+++ mongo_driver.py
@@ -2,15 +2,18 @@
 
 import pymongo
 
+'''
+A client class which uses pymongo driver to connect to Mongo Atlas Free Tier Account.
+'''
 
 class MongoClientConnection(object):
 
-	def __init__(self, db=None, collection=None):
+	def __init__(self, db=None, collection=None, username=None, pwd=None):
 		self.database = db
 		self.collection = collection
 		self.client = None
-		self.username = "s_admin_13"
-		self.pwd = "s_admin_13"
+		self.username = username
+		self.pwd = pwd
 		self.mongo_url = "mongodb://"+self.username+":" + self.pwd + "@cluster0-shard-00-00-kvfhu.mongodb.net:27017,cluster0-shard-00-01-kvfhu.mongodb.net:27017,cluster0-shard-00-02-kvfhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
 
 	def create_connection(self, database, collection):
@@ -23,3 +26,7 @@ class MongoClientConnection(object):
 
 	def write(self, data=None):
 		pass
+
+	def write_many(self, data=None):
+		pass
+
