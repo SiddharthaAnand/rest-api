b86484e9c946f97b404e8ef8342fb9f8dfdc729b
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-02-09 00:55:56 +0530

    Add testing, prod and dev configs

--- server/config.py
+++ server/config.py
@@ -1,9 +1,29 @@
+
+class Development(object):
+    """
+    Development environment configuration.
+    """
+    DEBUG = True
+    TESTING = False
+
+
+class Production(object):
+    """
+    Production environment configuration.
+    """
+    DEBUG = False
+    TESTING = False
+
+
 class Testing(object):
     """
     Development environment configuration.
     """
     TESTING = True
 
+
 app_config = {
-    'testing': Testing
+    'testing': Testing,
+    'production': Production,
+    'development': Development
 }
