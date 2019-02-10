b8b06cafa785195f099a2b2178449efb1a2e403e
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-02-10 21:10:42 +0530

    reformat the code and remove unused lines

--- server/models/read_credential.py
+++ server/models/read_credential.py
@@ -1,12 +1,11 @@
-'''
-method to read stored credentials from a file to connect to the mongo database.
-'''
-def read_credential(file_name=None):
 
+def read_credential(file_name=None):
+	"""
+	method to read stored credentials from a file to connect to the mongo database.
+	"""
 	_file = open(file_name, "r")
 	_lines = _file.readlines()
 	_file.close()
-	#print _lines
 	user_name = _lines[0].strip().split()[1]
 	pwd = _lines[1].strip().split()[1]
 	url = _lines[2].strip().split()[1]
